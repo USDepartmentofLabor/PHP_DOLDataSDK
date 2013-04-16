a2e5b02d0240ced49522fd8270c28d2e8d1e7e4d
Patrick K. Johnson Jr <johnson.patrick.k@dol.gov>
2013-04-16 13:33:06 -0400

    Updated PHPSDK code... 04/16/2013

--- GovAgenciesAPI.php
+++ GovAgenciesAPI.php
@@ -19,28 +19,28 @@ class GovSDK {
 	function procAPI($data) {
 		$post = $_POST;
 		
-		if ($post['GovAPI'] == 0) {
+		if ($post['API'] == 0) {
 			
 			// No API Key required to pull XML data...
-			$validatexml = "http://business.usa.gov/api/article/xml";
+			$validxml = "http://<agency>/<method>";
 			
-				$api = simplexml_load_file("$validatexml");
+				$api = simplexml_load_file("$validxml");
 				
 				foreach($api->children() as $child) {
-					$data = "{$child->title} {$child->learn_more_url} {$child->publish_date} {$child->detailed_text} {$child->topic}";
+					$data = "{$child->data}";
 				}
 			return $data;
 				
-		} elseif ($post['GovAPI'] == 1) {
+		} elseif ($post['API'] == 1) {
 			
 			 //API Key required to pull JSONP data...
-			$api = "http://api.census.gov/data/2010/acs5?key=<your key>&get=B25070_003E,NAME&for=county:*&in=state:06";
+			$api = "http://<agency>?key=<your key>&get=<method>";
 			
 			$ch = curl_init($api);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 			curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host:api.census.gov"));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host:<agency>"));
 			$data .= curl_exec($ch);
 			curl_close($ch);
 			
